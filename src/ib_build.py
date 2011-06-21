07c034bb788d81f6fa066cc1b020d83b97323a2f
sender <aidan.p.omahony@gmail.com>
2011-06-21 21:34:15 +1000

    added IB_DIR variable

--- src/ib_build.py
+++ src/ib_build.py
@@ -9,18 +9,23 @@ interactive brokers API directory:
  - (optional, swig 2.0+ required) swig -c++ -python -o swig_wrap.cpp -outdir .. ../swigify_ib.i
  - python ../ib_build.py build_ext --build-lib ..
  
+ 
  @author: Kieran O'Mahony
 """
 
 from distutils.core import setup, Extension
 
+IB_DIR = 'IB_964'
+
 ib_module = Extension('_ib',
-                      sources=['PosixSocketClient/EClientSocketBase.cpp', 
-                               'PosixSocketClient/EPosixClientSocket.cpp',
-                               'swig_wrap.cpp'],
-                      include_dirs=['.',
-                                    'PosixSocketClient',
-                                    'Shared'],
+                      sources=[IB_DIR + 
+                               '/PosixSocketClient/EClientSocketBase.cpp', 
+                               IB_DIR + 
+                               '/PosixSocketClient/EPosixClientSocket.cpp',
+                               IB_DIR + '/swig_wrap.cpp'],
+                      include_dirs=[IB_DIR,
+                                    IB_DIR + '/PosixSocketClient',
+                                    IB_DIR + '/Shared'],
                       define_macros=[ ('IB_USE_STD_STRING','1') ]
                       )
 
